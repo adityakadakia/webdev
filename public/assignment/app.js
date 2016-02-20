f028db621582a101bdbf1989266db3560925ca2d
adityakadakia <kadakia@ccs.neu.edu>
2016-02-20 15:19:13 -0500

    Nav bar on index page

---
+++ public/assignment/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Aditya on 2/19/2016.
+ */
+(function(){
+    angular
+        .module('FormBuilderApp',['ngRoute']);
+})();
---
+++ public/assignment/config.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Aditya on 2/20/2016.
+ */
+(function(){
+    angular
+        .module('FormBuilderApp');
+})();
