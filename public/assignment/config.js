524018abba68462f5a6860ecced74154aaef0148
adityakadakia <kadakia@ccs.neu.edu>
2016-02-20 18:49:43 -0500

    updated all views and added routes

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -3,5 +3,34 @@
  */
 (function(){
     angular
-        .module('FormBuilderApp');
+        .module('FormBuilderApp')
+        .config(configuration);
+
+    function configuration($routeProvider) {
+        $routeProvider
+            .when("/home", {
+                templateUrl: "views/home/home.view.html"
+            })
+            .when("/register", {
+                templateUrl: "views/users/register.view.html"
+            })
+            .when("/login",{
+                templateUrl: "views/users/login.view.html"
+            })
+            .when("/profile",{
+                templateUrl: "views/users/profile.view.html"
+            })
+            .when("/forms",{
+                templateUrl: "views/forms/forms.view.html"
+            })
+            .when("/fields",{
+                templateUrl: "views/forms/fields.view.html"
+            })
+            .when("/admin",{
+                templateUrl: "views/users/admin.view.html"
+            })
+            .otherwise({
+                redirectTo: "/home"
+            });
+    }
 })();
