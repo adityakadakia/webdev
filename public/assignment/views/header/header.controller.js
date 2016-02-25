c7932bb39eeda4379965f19f6852598b88e15518
Aditya Kadakia <kadakia@ccs.neu.edu>
2016-02-25 01:20:22 -0500

    header and sidebar fields ng-show, logout

--- public/assignment/views/header/header.controller.js
+++ public/assignment/views/header/header.controller.js
@@ -3,7 +3,13 @@
         .module("FormBuilderApp")
         .controller("HeaderController", HeaderController);
 
-    function HeaderController(){
+    function HeaderController($scope, $rootScope, $location) {
+        $scope.logOut = logOut;
 
+        function logOut() {
+            console.log("logout: ");
+            console.log($rootScope.user);
+            delete $rootScope.user;
+        }
     }
 })();
