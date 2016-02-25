79fcac32e6250138e4a3cd074f1a96dd30e0d3fe
Aditya Kadakia <kadakia@ccs.neu.edu>
2016-02-25 18:18:42 -0500

    redirection for pages requiring authentication

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -4,7 +4,27 @@
 (function () {
     angular
         .module('FormBuilderApp')
-        .config(configuration);
+        .config(configuration)
+        .run(redirect);
+
+    function redirect($rootScope, $location) {
+
+        // register listener to watch route changes
+        $rootScope.$on("$routeChangeStart", function (event, next, current) {
+            if ($rootScope.user == null) {
+                // no logged user, we should be going to #login
+                var url1 = next.templateUrl == "views/home/home.view.html";
+                var url2 = next.templateUrl == "views/users/register.view.html";
+                var url3 = next.templateUrl == "views/users/login.view.html";
+                if (url1 || url2 || url3) {
+                    // already going to #login, no redirect needed
+                } else {
+                    // not going to #login, we should redirect now
+                    $location.path("/home");
+                }
+            }
+        });
+    }
 
     function configuration($routeProvider) {
         $routeProvider
@@ -37,4 +57,5 @@
                 redirectTo: "/home"
             });
     }
+
 })();
