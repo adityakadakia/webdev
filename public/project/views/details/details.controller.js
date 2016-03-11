14cde884654d59966cd6e2b0cda70032170d7147
Aditya Kadakia <kadakia@ccs.neu.edu>
2016-03-10 21:54:34 -0500

    map on details page

--- public/project/views/details/details.controller.js
+++ public/project/views/details/details.controller.js
@@ -14,5 +14,24 @@
                 $scope.item = results[i];
         }
 
+        initMap();
+        function initMap() {
+            var bounds = new google.maps.LatLngBounds();
+            console.log("details: initMap");
+            var myLatLng = {lat: $scope.item.venue.location.lat, lng: $scope.item.venue.location.lng};
+
+            var map = new google.maps.Map(document.getElementById('details-map'), {
+                zoom: 13,
+                center: myLatLng
+            });
+
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+            });
+
+            bounds.extend(marker.getPosition());
+            //map.fitBounds(bounds);
+        }
     }
 })();
