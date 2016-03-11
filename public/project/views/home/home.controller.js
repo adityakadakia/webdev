9e8b6f600fc55500ee93e8c8967b8dd30e2755dc
Aditya Kadakia <kadakia@ccs.neu.edu>
2016-03-10 19:56:42 -0500

    map on home page

--- public/project/views/home/home.controller.js
+++ public/project/views/home/home.controller.js
@@ -10,11 +10,34 @@
         $scope.searchTerm;
         $scope.explorePlaces = explorePlaces;
         $scope.imageSize = "/800X500/";
+        $scope.initMap = initMap;
 
         function explorePlaces(searchTerm) {
             SearchService.explorePlace(searchTerm, function (places) {
                 $rootScope.results = places.response.groups[0].items;
+                initMap(places.response.groups[0].items);
             })
+
+        }
+
+        function initMap(myitems) {
+            var bounds = new google.maps.LatLngBounds();
+            console.log("initMap");
+            var map = new google.maps.Map(document.getElementById('map'), {
+                //zoom: 4,
+                //center: myLatLng
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+
+            for (i = 0; i < myitems.length; i++) {
+                var marker = new google.maps.Marker({
+                    position: {lat: myitems[i].venue.location.lat, lng: myitems[i].venue.location.lng},
+                    map: map,
+                    title: myitems[i].venue.name
+                })
+                bounds.extend(marker.getPosition());
+            }
+            map.fitBounds(bounds);
         }
     }
 })();
