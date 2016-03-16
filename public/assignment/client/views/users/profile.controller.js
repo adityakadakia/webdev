a664b1240f0b92b9e629b27d13c940799d8fe80f
Aditya Kadakia <kadakia@ccs.neu.edu>
2016-03-15 23:01:20 -0400

    scoped profile controller

--- public/assignment/client/views/users/profile.controller.js
+++ public/assignment/client/views/users/profile.controller.js
@@ -4,10 +4,13 @@
         .controller("ProfileController", ProfileController);
 
     function ProfileController($scope, $rootScope, UserService) {
-        $scope.user = $rootScope.user;
-        $scope.update = update;
+        var model = this;
+
+        model.user = $rootScope.user;
+        model.update = update;
 
         function update(user) {
+            console.log("ProfileController update");
             UserService.updateUser(user._id, user, function (u) {
                 $rootScope.user = u;
             })
