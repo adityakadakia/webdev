41ee51ef4a19ba694d82c81fd31d397cb94a5647
Aditya Kadakia <kadakia@ccs.neu.edu>
2016-03-15 23:05:23 -0400

    scoped register controller

--- public/assignment/client/views/users/register.controller.js
+++ public/assignment/client/views/users/register.controller.js
@@ -4,15 +4,18 @@
         .controller("RegisterController", RegisterController);
 
     function RegisterController($scope, $location,$rootScope, UserService) {
+        var model = this;
 
-        $scope.register = register;
+        model.register = register;
 
         function register(user) {
+            console.log("RegisterController register");
             console.log(user);
             UserService.findUserByUsername(user, doRegister);
         }
 
         function doRegister(user) {
+            console.log("RegisterController doRegister");
             if (user != null) {
                 console.log(user);
                 UserService.createUser(user, redirectUserToProfileIfValid);
@@ -22,6 +25,7 @@
         }
 
         function redirectUserToProfileIfValid(user) {
+            console.log("RegisterController redirectUserToProfileIfValid");
             if (user != null) {
                 $rootScope.user = user;
                 $location.url("/profile")
