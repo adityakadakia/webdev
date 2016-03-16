eb75474ff2c36673978cf862cca07612e993eaa6
Aditya Kadakia <kadakia@ccs.neu.edu>
2016-03-15 22:15:17 -0400

    changed directory structure

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -13,9 +13,9 @@
         $rootScope.$on("$routeChangeStart", function (event, next, current) {
             if ($rootScope.user == null) {
                 // no logged user, we should be going to #login
-                var url1 = next.templateUrl == "views/home/home.view.html";
-                var url2 = next.templateUrl == "views/users/register.view.html";
-                var url3 = next.templateUrl == "views/users/login.view.html";
+                var url1 = next.templateUrl == "client/views/home/home.view.html";
+                var url2 = next.templateUrl == "client/views/users/register.view.html";
+                var url3 = next.templateUrl == "client/views/users/login.view.html";
                 if (url1 || url2 || url3) {
                     // already going to #login, no redirect needed
                 } else {
@@ -29,29 +29,29 @@
     function configuration($routeProvider) {
         $routeProvider
             .when("/home", {
-                templateUrl: "views/home/home.view.html"
+                templateUrl: "client/views/home/home.view.html"
             })
             .when("/register", {
-                templateUrl: "views/users/register.view.html",
+                templateUrl: "client/views/users/register.view.html",
                 controller: "RegisterController"
             })
             .when("/login", {
-                templateUrl: "views/users/login.view.html",
+                templateUrl: "client/views/users/login.view.html",
                 controller: "LoginController"
             })
             .when("/profile", {
-                templateUrl: "views/users/profile.view.html",
+                templateUrl: "client/views/users/profile.view.html",
                 controller: "ProfileController"
             })
             .when("/forms", {
-                templateUrl: "views/forms/forms.view.html",
+                templateUrl: "client/views/forms/forms.view.html",
                 controller: "FormController"
             })
             .when("/fields", {
-                templateUrl: "views/forms/fields.view.html"
+                templateUrl: "client/views/forms/fields.view.html"
             })
             .when("/admin", {
-                templateUrl: "views/admin/admin.view.html"
+                templateUrl: "client/views/admin/admin.view.html"
             })
             .otherwise({
                 redirectTo: "/home"
