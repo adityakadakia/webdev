5416ce52dcefd714475f7f1bd691fa47b8c63899
Aditya Kadakia <kadakia@ccs.neu.edu>
2016-03-17 22:47:49 -0400

    updated app.js for field model and service

--- public/assignment/server/app.js
+++ public/assignment/server/app.js
@@ -4,9 +4,11 @@
 module.exports = function (app, uuid) {
 
     // pass db and mongoose reference to model
+    var fieldModel = require("./model/field.service.server.js")(uuid);
     var formModel = require("./models/form.model.server.js")(uuid);
     var userModel = require("./models/user.model.server.js")(uuid);
 
+    var formService = require("./services/field.service.server.js")(app, fieldModel);
     var formService = require("./services/form.service.server.js")(app, formModel);
     var userService = require("./services/user.service.server.js")(app, userModel);
 }
