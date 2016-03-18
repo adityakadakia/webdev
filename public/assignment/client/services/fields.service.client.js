af62eaeab7ae76eac6dc4cee820a146d575f653f
Aditya Kadakia <kadakia@ccs.neu.edu>
2016-03-17 23:16:50 -0400

    field service client

---
+++ public/assignment/client/services/fields.service.client.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Aditya on 3/17/2016.
+ */
+(function () {
+    angular
+        .module("FormBuilderApp")
+        .factory("FieldService", FieldService);
+
+    function FieldService($http) {
+        var api = {
+            findAllFieldsByFormId: findAllFieldsByFormId,
+            findFieldIdFormId: findFieldIdFormId,
+            deleteFieldIdFormId: deleteFieldIdFormId,
+            createFieldByFormId: createFieldByFormId,
+            updateFieldIdFormId: updateFieldIdFormId
+        }
+
+        return api;
+
+        function findAllFieldsByFormId(formId) {
+            return $http.get("/api/assignment/form/" + formId + "/field");
+        }
+
+        function findFieldIdFormId(formId, fieldId) {
+            return $http.get("/api/assignment/form/" + formId + "/field/" + fieldId);
+        }
+
+        function deleteFieldIdFormId(formId, fieldId) {
+            return $http.delete(" /api/assignment/form/" + formId + "/field/" + fieldId);
+        }
+
+        function createFieldByFormId(formId, field) {
+            return $http.post("api/assignment/form/" + formId + "/field", field);
+        }
+
+        function updateFieldIdFormId(formId, fieldId, field) {
+            return $http.put("/api/assignment/form/" + formId + "/field/" + fieldId, field);
+        }
+    }
+})();
