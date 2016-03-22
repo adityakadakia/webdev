f673853d29730b74bbaa99aff145bb1f89cc3d7d
Aditya Kadakia <kadakia@ccs.neu.edu>
2016-03-22 17:14:21 -0400

    scoped details controller

--- public/project/client/views/details/details.controller.js
+++ public/project/client/views/details/details.controller.js
@@ -7,25 +7,26 @@
         .module("Voyager")
         .controller("DetailsController", DetailsController);
 
-    function DetailsController($routeParams, $scope, $rootScope, SearchService, ReviewService, UserService) {
+    function DetailsController($routeParams, $rootScope, SearchService, ReviewService, UserService) {
+        var model = this;
         var id = $routeParams.id;
         var results = $rootScope.results;
-        $scope.selected = -1;
-        $scope.deleteReview = deleteReview;
-        $scope.addReview = addReview;
-        $scope.selectReview = selectReview;
-        $scope.updateReview = updateReview;
-        $scope.findFullUserNamebyUserId = findFullUserNamebyUserId;
+        model.selected = -1;
+        model.deleteReview = deleteReview;
+        model.addReview = addReview;
+        model.selectReview = selectReview;
+        model.updateReview = updateReview;
+        model.findFullUserNamebyUserId = findFullUserNamebyUserId;
 
         for (var i in results) {
             if (results[i].venue.id == id)
-                $scope.item = results[i];
+                model.item = results[i];
         }
 
         initMap();
-        console.log("placeId: " + $scope.item.venue.id);
-        findAllReviewsByPlaceId($scope.item.venue.id);
-        console.log($scope.reviews);
+        console.log("placeId: " + model.item.venue.id);
+        findAllReviewsByPlaceId(model.item.venue.id);
+        console.log(model.reviews);
 
         function findFullUserNamebyUserId(userId) {
             return UserService.userIdtoFullUserName(userId);
@@ -33,54 +34,55 @@
 
         function findAllReviewsByPlaceId(placeId) {
             ReviewService.findAllReviewsByPlaceId(placeId, function (response) {
-                $scope.reviews = response;
+                model.reviews = response;
             });
         }
 
         function selectReview(index) {
-            $scope.selected = index;
+            model.selected = index;
             var editReview = {
-                "_id": $scope.reviews[index]["_id"],
-                "title": $scope.reviews[index]["title"],
-                "description": $scope.reviews[index]["description"],
-                "timestamp": $scope.reviews[index]["timestamp"],
-                "placeId": $scope.reviews[index]["placeId"],
-                "userId": $scope.reviews[index]["userId"],
-                "commentIds": $scope.reviews[index]["commentIds"]
+                "_id": model.reviews[index]["_id"],
+                "title": model.reviews[index]["title"],
+                "description": model.reviews[index]["description"],
+                "timestamp": model.reviews[index]["timestamp"],
+                "placeId": model.reviews[index]["placeId"],
+                "userId": model.reviews[index]["userId"],
+                "commentIds": model.reviews[index]["commentIds"]
             }
-            $scope.review = editReview;
-            $scope.editReview = editReview;
+            model.review = editReview;
+            model.editReview = editReview;
         }
 
         function addReview(review) {
-            ReviewService.addReview(review, $scope.item.venue.id, function () {
-                $scope.review = {};
-                $scope.selected = -1;
-                findAllReviewsByPlaceId($scope.item.venue.id);
+            review.title = "";
+            ReviewService.addReview(review, model.item.venue.id, function () {
+                model.review = {};
+                model.selected = -1;
+                findAllReviewsByPlaceId(model.item.venue.id);
             });
         }
 
         function updateReview(review) {
             ReviewService.updateReview(review, function (newReview) {
-                $scope.reviews[$scope.selected] = newReview;
-                $scope.review = {};
-                $scope.selected = -1;
+                model.reviews[model.selected] = newReview;
+                model.review = {};
+                model.selected = -1;
             });
         }
 
         function deleteReview(index) {
-            var reviewId = $scope.reviews[index]._id;
+            var reviewId = model.reviews[index]._id;
             ReviewService.deleteReview(reviewId, function () {
-                $scope.review = {};
-                $scope.selected = -1;
-                findAllReviewsByPlaceId($scope.item.venue.id);
+                model.review = {};
+                model.selected = -1;
+                findAllReviewsByPlaceId(model.item.venue.id);
             });
         }
 
         function initMap() {
             var bounds = new google.maps.LatLngBounds();
             console.log("details: initMap");
-            var myLatLng = {lat: $scope.item.venue.location.lat, lng: $scope.item.venue.location.lng};
+            var myLatLng = {lat: model.item.venue.location.lat, lng: model.item.venue.location.lng};
 
             var map = new google.maps.Map(document.getElementById('details-map'), {
                 zoom: 13,
