fe45ac05f24a9d391b8d67cedb8e006ce4d52a62
Aditya Kadakia <kadakia@ccs.neu.edu>
2016-03-23 13:28:51 -0400

    scoped profile controller

--- public/project/client/views/users/profile/profile.controller.js
+++ public/project/client/views/users/profile/profile.controller.js
@@ -8,30 +8,17 @@
         .controller("ProfileController", ProfileController);
 
     function ProfileController($rootScope, $scope, UserService) {
-        console.log("ProfileController");
+        var model = this;
 
-        $scope.update = update;
-        $scope.userprofile = {
-            "_id": $rootScope.user._id,
-            "firstName": $rootScope.user.firstName,
-            "lastName": $rootScope.user.lastName,
-            "username": $rootScope.user.username,
-            "password": $rootScope.user.password,
-            "roles": $rootScope.user.roles
-        }
-        console.log($scope.userprofile);
+        model.update = update;
+        model.userprofile = $rootScope.user;
+
+        console.log("ProfileController" + JSON.stringify(model.userprofile));
 
         function update(userprofile) {
             UserService.updateUser(userprofile._id, userprofile, function (users) {
                 if (users != null)
-                    $rootScope.user = {
-                        "_id": $rootScope.user._id,
-                        "firstName": userprofile.firstName,
-                        "lastName": userprofile.lastName,
-                        "username": userprofile.username,
-                        "password": userprofile.password,
-                        "roles": userprofile.roles
-                    }
+                    $rootScope.user = model.userprofile
             });
         }
     }
