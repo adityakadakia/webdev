3701d3aa8fe0a627bce60a7ffee2619f5a062226
Aditya Kadakia <kadakia@ccs.neu.edu>
2016-03-23 15:32:57 -0400

    sign out

---
+++ public/project/client/views/header/header.controller.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Aditya on 3/23/2016.
+ */
+"use strict";
+(function () {
+    angular
+        .module("Voyager")
+        .controller("HeaderController", HeaderController);
+
+    function HeaderController($scope, $rootScope, $location, UserService) {
+        var model = this;
+        model.logOut = logOut;
+
+        function logOut() {
+            console.log("HeaderController logOut");
+            UserService
+                .logOut()
+                .then(function (response) {
+                    console.log(response.data);
+                    $rootScope.user = undefined;
+                    $location.url("/home");
+                });
+        }
+    }
+})();
