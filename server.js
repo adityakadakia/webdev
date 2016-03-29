d621ab2d149f84bf507f29888a5b97ce81bb6c8f
adityakadakia <kadakia@ccs.neu.edu>
2016-03-29 18:32:03 -0400

    added db connection string to server.js

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ var multer = require('multer');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var uuid = require('node-uuid');
+var mongoose      = require('mongoose');
+
+var connectionString = 'mongodb://127.0.0.1:27017/webdev';
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+var db = mongoose.connect(connectionString);
 
 var app = express();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(multer());
