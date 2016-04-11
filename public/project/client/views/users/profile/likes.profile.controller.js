7e89dc743ecf3b25c314f564b34e094e46aea79b
Aditya Kadakia <kadakia@ccs.neu.edu>
2016-04-11 16:49:12 -0400

    added sidebar to profile pages

---
+++ public/project/client/views/users/profile/likes.profile.controller.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Aditya on 4/11/2016.
+ */
+"use strict";
+(function () {
+    angular
+        .module("Voyager")
+        .controller("LikesController", LikesController);
+
+    function LikesController() {
+
+    }
+
+})();
