4f201e1e40c185271dfd62df3896c015ae305464
Aditya Kadakia <kadakia@ccs.neu.edu>
2016-04-11 23:16:11 -0400

    view followers of a user

--- public/project/client/views/users/profile/followers.profile.controller.js
+++ public/project/client/views/users/profile/followers.profile.controller.js
@@ -8,6 +8,35 @@
         .controller("FollowerController", FollowerController);
 
     function FollowerController($routeParams, $location, UserService) {
+        var model = this;
+        var userId = $routeParams.userId;
+        model.followers = [];
+        console.log("userId: " + userId);
 
+        if (userId) {
+            UserService
+                .userIdtoUser(userId)
+                .then(function (response) {
+                    initUserProfile(response.data);
+                });
+        } else {
+            UserService
+                .getCurrentUser()
+                .then(function (response) {
+                    $location.url('/followers/' + response.data._id);
+                });
+        }
+
+        function initUserProfile(u) {
+            model.userProfile = u;
+            for (var i in model.userProfile.followers) {
+                UserService
+                    .userIdtoUser(model.userProfile.followers[i])
+                    .then(function (response) {
+                        model.followers.push(response.data);
+                    });
+            }
+            console.log("user: " + JSON.stringify(model.userProfile));
+        }
     }
 })();
