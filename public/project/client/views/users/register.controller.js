23d4b62dd9b85cc74cf670c489d0f1a03d7b529d
Aditya Kadakia <kadakia@ccs.neu.edu>
2016-04-16 17:29:59 -0400

    added input validations

--- public/project/client/views/users/register.controller.js
+++ public/project/client/views/users/register.controller.js
@@ -12,34 +12,53 @@
         model.register = register;
         model.duplicateUser = false;
 
+        function validateUser(user) {
+            var flag = true;
+            flag = flag && user.username;
+            flag = flag && user.password;
+            flag = flag && user.firstName;
+            flag = flag && user.lastName;
+            flag = flag && user.emails;
+            if (user.password == user.verifyPassword)
+                flag = flag && true;
+            else
+                flag = flag && false;
+            return flag;
+        }
+
         function register(user) {
             var users;
             console.log("RegisterController register");
             console.log("Submitted user: " + JSON.stringify(user));
-            UserService.register(user)
-                .then(function (response) {
-                        users = response.data;
-                        if (users) {
-                            UserService
-                                .findUserByUsername(user.username)
-                                .then(function (res) {
-                                        var usr = res.data;
-                                        console.log("Registered user: " + JSON.stringify(usr));
-                                        if (usr) {
-                                            UserService.setCurrentUser(usr);
-                                            $location.url("/profile");
-                                        }
-                                    },
-                                    function (err) {
-                                        console.log(err.data);
-                                    });
-                        }
-                    },
-                    function (err) {
-                        console.log(err.data);
-                        if (err.data == "Duplicate User")
-                            model.duplicateUser = true;
-                    });
+            var flag = validateUser(user);
+            if (flag == true)
+                UserService.register(user)
+                    .then(function (response) {
+                            users = response.data;
+                            if (users) {
+                                UserService
+                                    .findUserByUsername(user.username)
+                                    .then(function (res) {
+                                            var usr = res.data;
+                                            console.log("Registered user: " + JSON.stringify(usr));
+                                            if (usr) {
+                                                UserService.setCurrentUser(usr);
+                                                $location.url("/profile");
+                                            }
+                                        },
+                                        function (err) {
+                                            console.log(err.data);
+                                        });
+                            }
+                        },
+                        function (err) {
+                            console.log(err.data);
+                            if (err.data == "Duplicate User")
+                                model.duplicateUser = true;
+                        });
+            else {
+                alert("Please verify your details.");
+            }
         }
     }
 })();
