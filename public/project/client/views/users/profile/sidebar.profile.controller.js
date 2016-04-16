04db0720516148a1e744eefb4521e3bb5b391be3
Aditya Kadakia <kadakia@ccs.neu.edu>
2016-04-15 21:13:03 -0400

    icons to the side bar

---
+++ public/project/client/views/users/profile/sidebar.profile.controller.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Aditya on 4/15/2016.
+ */
+"use strict";
+(function () {
+    angular
+        .module("Voyager")
+        .controller("SidebarController", SidebarController);
+
+    function SidebarController($location) {
+
+    }
+})();
